718f7b6db0b11ddd7e1603be71b275652a52df5d
deandeanazulay <139657917+deandeanazulay@users.noreply.github.com>
2025-09-22 19:17:50 -0500

    Updated ProfileScreen.tsx

--- src/components/screens/ProfileScreen.tsx
+++ src/components/screens/ProfileScreen.tsx
@@ -170,6 +170,8 @@ export default function ProfileScreen({ selectedEgoState, onEgoStateChange }: Pr
           </div>
         </div>
       </div>
+    )
+    }
     </div>
   );
             </div>
