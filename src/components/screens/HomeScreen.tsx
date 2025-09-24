93213454fd4351e1b6a4291f2627f1cf78ff18e8
deandeanazulay <139657917+deandeanazulay@users.noreply.github.com>
2025-09-23 23:48:08 -0500

    Fix undefined handleOrbClick function

--- src/components/screens/HomeScreen.tsx
+++ src/components/screens/HomeScreen.tsx
@@ -117,7 +117,7 @@ export default function HomeScreen({
                 }}
               >
                 <Orb
-                  onTap={handleOrbClick}
+                  onTap={onOrbTap}
                   afterglow={false}
                   egoState={activeEgoState}
                   size={window.innerWidth < 768 ? 450 : 540}
