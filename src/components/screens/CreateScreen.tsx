e3170d84593e97cb5014010bf93b18306024a00e
deandeanazulay <139657917+deandeanazulay@users.noreply.github.com>
2025-09-24 00:30:38 -0500

    Updated CreateScreen.tsx

--- src/components/screens/CreateScreen.tsx
+++ src/components/screens/CreateScreen.tsx
@@ -682,7 +682,7 @@ export default function CreateScreen({ onProtocolCreate, onShowAuth }: CreateScr
             <Orb
               onTap={() => {}}
               egoState={activeEgoState}
-              size={1000}
+              size={150}
               variant="webgl"
             />
           </div>
