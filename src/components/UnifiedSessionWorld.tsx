d14583c29078e1aecc599fba10bcaafbf85d1f22
deandeanazulay <139657917+deandeanazulay@users.noreply.github.com>
2025-09-25 05:27:55 -0500

    Updated UnifiedSessionWorld.tsx

--- src/components/UnifiedSessionWorld.tsx
+++ src/components/UnifiedSessionWorld.tsx
@@ -212,7 +212,7 @@ export default function UnifiedSessionWorld({ sessionConfig, onComplete, onCance
         sessionManager.dispose();
       }
     };
-  }, [sessionConfig, showToast]);
+  }, []);
 
   const handleSessionComplete = async () => {
     console.log('[SESSION] Session completed');
